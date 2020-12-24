09fa5d0bb9d4ca4d80c25b2a34b9322e93b106af
Felice <felice.estermann@gmx.ch>
2020-12-24 11:37:54 +0100

    add cascade_rcnn_r50_caffe config

---
+++ configs/raubtierv2/cascade_rcnn_r50_caffe_fpn_1x_coco_raubtierv2.py
@@ -0,0 +1,81 @@
+# The new config inherits a base config to highlight the necessary modification
+_base_ = '../cascade_rcnn/cascade_rcnn_r50_caffe_fpn_1x_coco.py'
+
+# We also need to change the num_classes in head to match the dataset's annotation
+model = dict(
+    roi_head=dict(
+        bbox_head=[
+            dict(
+                type='Shared2FCBBoxHead',
+                in_channels=256,
+                fc_out_channels=1024,
+                roi_feat_size=7,
+                num_classes=3,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=[0., 0., 0., 0.],
+                    target_stds=[0.1, 0.1, 0.2, 0.2]),
+                reg_class_agnostic=True,
+                loss_cls=dict(
+                    type='CrossEntropyLoss',
+                    use_sigmoid=False,
+                    loss_weight=1.0),
+                loss_bbox=dict(type='SmoothL1Loss', beta=1.0,
+                               loss_weight=1.0)),
+            dict(
+                type='Shared2FCBBoxHead',
+                in_channels=256,
+                fc_out_channels=1024,
+                roi_feat_size=7,
+                num_classes=3,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=[0., 0., 0., 0.],
+                    target_stds=[0.05, 0.05, 0.1, 0.1]),
+                reg_class_agnostic=True,
+                loss_cls=dict(
+                    type='CrossEntropyLoss',
+                    use_sigmoid=False,
+                    loss_weight=1.0),
+                loss_bbox=dict(type='SmoothL1Loss', beta=1.0,
+                               loss_weight=1.0)),
+            dict(
+                type='Shared2FCBBoxHead',
+                in_channels=256,
+                fc_out_channels=1024,
+                roi_feat_size=7,
+                num_classes=3,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=[0., 0., 0., 0.],
+                    target_stds=[0.033, 0.033, 0.067, 0.067]),
+                reg_class_agnostic=True,
+                loss_cls=dict(
+                    type='CrossEntropyLoss',
+                    use_sigmoid=False,
+                    loss_weight=1.0),
+                loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0))
+        ]
+        
+        
+        ))
+
+# Modify dataset related settings
+dataset_type = 'COCODataset'
+classes = ('luchs', 'rotfuchs', 'wolf')
+data = dict(
+    train=dict(
+        img_prefix='customData/train/',
+        classes=classes,
+        ann_file='customData/train/_annotations.coco.json'),
+    val=dict(
+        img_prefix='customData/valid/',
+        classes=classes,
+        ann_file='customData/valid/_annotations.coco.json'),
+    test=dict(
+        img_prefix='customData/test/',
+        classes=classes,
+        ann_file='customData/test/_annotation.coco.json'))
+
+# Use the pre-trained model to obtain higher performance
+load_from = 'checkpoints/cascade_rcnn_r50_caffe_fpn_1x_coco_bbox_mAP-0.404_20200504_174853-b857be87.pth'
