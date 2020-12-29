648abac13dd784c2306a817cd9373b5afffba9f7
Felice <felice.estermann@gmx.ch>
2020-12-29 16:53:24 +0100

    add cascade_rcnn_r101_fpn_dconv_c3-c5 config

---
+++ configs/raubtierv2/cascade_rcnn_r101_fpn_dconv_c3-c5_1x_coco_raubtierv2.py
@@ -0,0 +1,25 @@
+# The new config inherits a base config to highlight the necessary modification
+_base_ = '../dcn/cascade_rcnn_r101_fpn_dconv_c3-c5_1x_coco.py'
+
+# Modify dataset related settings
+dataset_type = 'COCODataset'
+classes = ('luchs', 'rotfuchs', 'wolf')
+data = dict(
+    train=dict(
+        img_prefix='customData/train/',
+        classes=classes,
+        ann_file='customData/train/_annotations.coco.json'),
+    val=dict(
+        img_prefix='customData/valid/',
+        classes=classes,
+        ann_file='customData/valid/_annotations.coco.json'),
+    test=dict(
+        img_prefix='customData/test/',
+        classes=classes,
+        ann_file='customData/test/_annotation.coco.json'))
+
+
+# Use the pre-trained model to obtain higher performance
+load_from = 'checkpoints/cascade_rcnn_r101_fpn_dconv_c3-c5_1x_coco_20200203-3b2f0594.pth'
+
+#http://download.openmmlab.com/mmdetection/v2.0/dcn/cascade_rcnn_r101_fpn_dconv_c3-c5_1x_coco/cascade_rcnn_r101_fpn_dconv_c3-c5_1x_coco_20200203-3b2f0594.pth
