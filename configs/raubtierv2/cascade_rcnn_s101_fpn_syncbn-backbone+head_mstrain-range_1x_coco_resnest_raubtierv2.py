4467fe3dc4d3d574086a99c6ad22eefac49ad092
Felice <felice.estermann@gmx.ch>
2020-12-30 13:22:39 +0100

    add cascade_rcnn_s101_fpn_syncbn-backbone+head_mstrain-range_1x_coco_resnest_raubtierv2.py

---
+++ configs/raubtierv2/cascade_rcnn_s101_fpn_syncbn-backbone+head_mstrain-range_1x_coco_resnest_raubtierv2.py
@@ -0,0 +1,94 @@
+# Basiskonfigurationsfile
+_base_ = '../resnest/cascade_rcnn_s101_fpn_syncbn-backbone+head_mstrain-range_1x_coco.py'
+
+model = dict(
+	roi_head=dict(
+	
+		bbox_head=[
+            dict(
+                type='Shared4Conv1FCBBoxHead',
+                in_channels=256,
+                conv_out_channels=256,
+                fc_out_channels=1024,
+                norm_cfg=dict(type='SyncBN', requires_grad=True),
+                roi_feat_size=7,
+                num_classes=80,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=[0.0, 0.0, 0.0, 0.0],
+                    target_stds=[0.1, 0.1, 0.2, 0.2]),
+                reg_class_agnostic=True,
+                loss_cls=dict(
+                    type='CrossEntropyLoss',
+                    use_sigmoid=False,
+                    loss_weight=1.0),
+                loss_bbox=dict(type='SmoothL1Loss', beta=1.0,
+                               loss_weight=1.0)),
+            dict(
+                type='Shared4Conv1FCBBoxHead',
+                in_channels=256,
+                conv_out_channels=256,
+                fc_out_channels=1024,
+                norm_cfg=dict(type='SyncBN', requires_grad=True),
+                roi_feat_size=7,
+                num_classes=80,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=[0.0, 0.0, 0.0, 0.0],
+                    target_stds=[0.05, 0.05, 0.1, 0.1]),
+                reg_class_agnostic=True,
+                loss_cls=dict(
+                    type='CrossEntropyLoss',
+                    use_sigmoid=False,
+                    loss_weight=1.0),
+                loss_bbox=dict(type='SmoothL1Loss', beta=1.0,
+                               loss_weight=1.0)),
+            dict(
+                type='Shared4Conv1FCBBoxHead',
+                in_channels=256,
+                conv_out_channels=256,
+                fc_out_channels=1024,
+                norm_cfg=dict(type='SyncBN', requires_grad=True),
+                roi_feat_size=7,
+                num_classes=80,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=[0.0, 0.0, 0.0, 0.0],
+                    target_stds=[0.033, 0.033, 0.067, 0.067]),
+                reg_class_agnostic=True,
+                loss_cls=dict(
+                    type='CrossEntropyLoss',
+                    use_sigmoid=False,
+                    loss_weight=1.0),
+                loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0))
+        	]
+	
+	
+	)
+
+
+)
+
+# Modify dataset related settings
+dataset_type = 'COCODataset'
+classes = ('luchs', 'rotfuchs', 'wolf')
+data = dict(
+    train=dict(
+        img_prefix='customData/train/',
+        classes=classes,
+        ann_file='customData/train/_annotations.coco.json'),
+    val=dict(
+        img_prefix='customData/valid/',
+        classes=classes,
+        ann_file='customData/valid/_annotations.coco.json'),
+    test=dict(
+        img_prefix='customData/test/',
+        classes=classes,
+        ann_file='customData/test/_annotation.coco.json'))
+
+# Pretrained model laden
+
+load_from = 'checkpoints/cascade_rcnn_s101_fpn_syncbn-backbone%2Bhead_mstrain-range_1x_coco_20201005_113242-b9459f8f.pth'
+
+#http://download.openmmlab.com/mmdetection/v2.0/resnest/cascade_rcnn_s101_fpn_syncbn-backbone%2Bhead_mstrain-range_1x_coco/cascade_rcnn_s101_fpn_syncbn-backbone%2Bhead_mstrain-range_1x_coco_20201005_113242-b9459f8f.pth
+
