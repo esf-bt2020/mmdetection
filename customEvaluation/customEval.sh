7cf84b00ad236717c3aae9c89a5a86d9f15c5fa2
Felice <felice.estermann@gmx.ch>
2021-01-18 12:17:41 +0100

    add customEvaluation

---
+++ customEvaluation/customEval.sh
@@ -0,0 +1,63 @@
+
+#ResNest
+#configfile="./configs/raubtierv2/cascade_rcnn_s101_fpn_syncbn-backbone+head_mstrain-range_1x_coco_resnest_raubtierv2.py"
+#checkpointfile="./customEvaluation/resnest_20201230_epoch_12_x62001.pth"
+#logfile="work_dirs/cascade_rcnn_s101_fpn_syncbn-backbone+head_mstrain-range_1x_coco_resnest_raubtierv2_20201230/20201230_132904.log.json"
+
+#Centripetal
+#configfile="./configs/raubtierv2/centripetalnet_hourglass104_mstest_16x6_210e_coco_raubtierv2.py"
+#checkpointfile="/media/felice/RED/FFHS_BT/_TrainedModels/PC03LIN_mmdetection/centripetalnet_hourglass104_mstest_16x6_210e_coco_raubtierv2/epoch_11.pth"
+#logfile="/media/felice/RED/FFHS_BT/_TrainedModels/PC03LIN_mmdetection/centripetalnet_hourglass104_mstest_16x6_210e_coco_raubtierv2/20201230_192052copy.log.json"
+
+# DCN
+#configfile="./configs/raubtierv2/cascade_rcnn_r101_fpn_dconv_c3-c5_1x_coco_raubtierv2.py"
+#checkpointfile="/media/felice/RED/FFHS_BT/_TrainedModels/PC03LIN_mmdetection/cascade_rcnn_r101_fpn_dconv_c3-c5_1x_coco_raubtierv2_20200104_30ep/epoch_27.pth"
+#logfile="customEvaluation/cascade_rcnn_r101_fpn_dconv_c3-c5_1x_coco_raubtierv2_20210104_163131.log.json"
+
+# DetectoRS
+#configfile="./configs/raubtierv2/detectors_cascade_rcnn_r50_1x_coco_raubtierv2.py"
+#checkpointfile="/media/felice/RED/FFHS_BT/_TrainedModels/detectors_cascade_rcnn_r50_esfDSVM01B_20201231/detectors_cascade_rcnn_r50_1x_coco_raubtierv2/epoch_11.pth"
+#logfile="/media/felice/RED/FFHS_BT/_TrainedModels/detectors_cascade_rcnn_r50_esfDSVM01B_20201231/detectors_cascade_rcnn_r50_1x_coco_raubtierv2//20201230_161644.log.json"
+
+# HRNet
+configfile="./configs/raubtierv2/cascade_rcnn_hrnetv2p_w40_20e_coco_raubtierv2.py"
+checkpointfile="/media/felice/RED/FFHS_BT/_TrainedModels/colabwork-2021-01-0418-09-09_hrnet/content/esf-bt2020_mmdetection/work_dirs/cascade_rcnn_hrnetv2p_w40_20e_coco_raubtierv2/epoch_14.pth"
+logfile="/media/felice/RED/FFHS_BT/_TrainedModels/colabwork-2021-01-0418-09-09_hrnet/content/esf-bt2020_mmdetection/work_dirs/cascade_rcnn_hrnetv2p_w40_20e_coco_raubtierv2/20210104_145037.log.json"
+
+# GA Faster
+#configfile="./configs/raubtierv2/ga_faster_x101_64x4d_fpn_1x_coco_raubtierv2.py"
+#checkpointfile="/media/felice/RED/FFHS_BT/_TrainedModels/detectors_cascade_rcnn_r50_esfDSVM01B_20201231/detectors_cascade_rcnn_r50_1x_coco_raubtierv2/epoch_11.pth"
+#logfile="/media/felice/RED/FFHS_BT/_TrainedModels/detectors_cascade_rcnn_r50_esfDSVM01B_20201231/detectors_cascade_rcnn_r50_1x_coco_raubtierv2/20201230_161644.log.json"
+
+# RepPoints
+#configfile="./configs/raubtierv2/reppoints_moment_x101_fpn_dconv_c3-c5_gn-neck+head_2x_coco_raubtierv2.py"
+#checkpointfile="work_dirs/reppoints_moment_x101_fpn_dconv_c3-c5_gn-neck+head_2x_coco_raubtierv2_20201229/epoch_24.pth"
+#logfile="work_dirs/reppoints_moment_x101_fpn_dconv_c3-c5_gn-neck+head_2x_coco_raubtierv2_20201229/20201229_225252.log.json"
+
+# GFL
+#configfile="./configs/raubtierv2/gfl_r101_fpn_dconv_c3-c5_mstrain_2x_coco_raubtierv2.py"
+#checkpointfile="/media/felice/RED/FFHS_BT/_TrainedModels/detectors_cascade_rcnn_r50_esfDSVM01B_20201231/detectors_cascade_rcnn_r50_1x_coco_raubtierv2/epoch_11.pth"
+#logfile="/media/felice/RED/FFHS_BT/_TrainedModels/detectors_cascade_rcnn_r50_esfDSVM01B_20201231/detectors_cascade_rcnn_r50_1x_coco_raubtierv2/20201230_161644.log.json"
+
+cd ..
+
+#Evaluierung auf Testdatenset
+#python ./tools/test.py $configfile $checkpointfile --eval bbox
+ 
+#Training => bbox_mAP plotten
+#echo python tools/analyze_logs.py plot_curve $logfile --keys bbox_mAP #–-legend bbox_mAP
+python tools/analyze_logs.py plot_curve $logfile --keys bbox_mAP #–-legend bbox_mAP
+ 
+
+#Training => loss_cls und loss_bbox plotten
+#python tools/analyze_logs.py plot_curve $logfile --keys loss_cls loss_bbox --legend loss_cls loss_bbox
+ 
+
+#Testdaten anzeigen
+
+# python ./tools/test.py \
+# $configfile \
+# $checkpointfile \
+# --out resultfile.pkl \
+# --show
+
