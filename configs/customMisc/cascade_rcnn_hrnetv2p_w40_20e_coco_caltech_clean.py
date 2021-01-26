5b9a9d047a81f001c793b8d6a16c1e44c65a6356
Felice <felice.estermann@gmx.ch>
2021-01-27 00:03:42 +0100

    add hrnet caltech clean config

---
+++ configs/customMisc/cascade_rcnn_hrnetv2p_w40_20e_coco_caltech_clean.py
@@ -0,0 +1,85 @@
+# Basiskonfigurationsfile
+_base_ = '../hrnet/cascade_rcnn_hrnetv2p_w40_20e_coco.py'
+
+
+# Modify dataset related settings
+dataset_type = 'CaltechDataset'
+classes = ('bobcat', 'opossum', 'empty', 'coyote', 'racoon', 'bird', 'dog', 'cat', 'squirrel', 'rabbit', 'skunk', 'lizard', 'rodent', 'badger', 'deer', 'cow', 'car', 'fox', 'pig','mountain_lion', 'bat', 'insect')
+
+model = dict(
+	roi_head=dict(
+		        bbox_head=[
+            dict(
+                type='Shared2FCBBoxHead',
+                in_channels=256,
+                fc_out_channels=1024,
+                roi_feat_size=7,
+                num_classes=22,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=[0.0, 0.0, 0.0, 0.0],
+                    target_stds=[0.1, 0.1, 0.2, 0.2]),
+                reg_class_agnostic=True,
+                loss_cls=dict(
+                    type='CrossEntropyLoss',
+                    use_sigmoid=False,
+                    loss_weight=1.0),
+                loss_bbox=dict(type='SmoothL1Loss', beta=1.0,
+                               loss_weight=1.0)),
+            dict(
+                type='Shared2FCBBoxHead',
+                in_channels=256,
+                fc_out_channels=1024,
+                roi_feat_size=7,
+                num_classes=22,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=[0.0, 0.0, 0.0, 0.0],
+                    target_stds=[0.05, 0.05, 0.1, 0.1]),
+                reg_class_agnostic=True,
+                loss_cls=dict(
+                    type='CrossEntropyLoss',
+                    use_sigmoid=False,
+                    loss_weight=1.0),
+                loss_bbox=dict(type='SmoothL1Loss', beta=1.0,
+                               loss_weight=1.0)),
+            dict(
+                type='Shared2FCBBoxHead',
+                in_channels=256,
+                fc_out_channels=1024,
+                roi_feat_size=7,
+                num_classes=22,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=[0.0, 0.0, 0.0, 0.0],
+                    target_stds=[0.033, 0.033, 0.067, 0.067]),
+                reg_class_agnostic=True,
+                loss_cls=dict(
+                    type='CrossEntropyLoss',
+                    use_sigmoid=False,
+                    loss_weight=1.0),
+                loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0))
+        ]
+	
+	)
+
+)
+
+data = dict(
+    train=dict(
+    	type='CaltechDataset',
+        img_prefix='/media/SSD2project/WilLiCam/datasets/caltech/cct_images',
+        classes=classes,
+        ann_file='customDataCaltech/caltech_adv/eccv_train_adv.json'),
+    val=dict(
+    	type='CaltechDataset',
+        img_prefix='/media/SSD2project/WilLiCam/datasets/caltech/cct_images',
+        classes=classes,
+        ann_file='customDataCaltech/caltech_adv/eccv_val_adv.json'),
+    test=dict(
+    	type='CaltechDataset',
+         img_prefix='/media/SSD2project/WilLiCam/datasets/caltech/cct_images',
+        classes=classes,
+        ann_file='customDataCaltech/caltech_adv/eccv_val_TEST_adv.json'))
+
+
