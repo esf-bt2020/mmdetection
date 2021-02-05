644a5e3a0cef0e9eb4f5cbbb14682bfe4d216384
Felice <felice.estermann@gmx.ch>
2021-02-05 13:33:38 +0100

    gpu selection changed (cheetah)

--- tools/dist_train_gpu123.sh
+++ tools/dist_train_gpu123.sh
@@ -4,8 +4,7 @@ CONFIG=$1
 GPUS=$2
 PORT=${PORT:-29500}
 
-CUDA_VISIBLE_DEVICES=2,3,4
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+CUDA_VISIBLE_DEVICES=1,2,3 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
     $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3}
