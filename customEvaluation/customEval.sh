f8eb032dc42db3b6df1ab5e18b63a9cf05384259
Felice <felice.estermann@gmx.ch>
2021-02-09 11:04:06 +0100

    modify customEvaluation

--- customEvaluation/customEval.sh
+++ customEvaluation/customEval.sh
@@ -9,20 +9,34 @@
 #checkpointfile="/media/felice/RED/FFHS_BT/_TrainedModels/PC03LIN_mmdetection/centripetalnet_hourglass104_mstest_16x6_210e_coco_raubtierv2/epoch_11.pth"
 #logfile="/media/felice/RED/FFHS_BT/_TrainedModels/PC03LIN_mmdetection/centripetalnet_hourglass104_mstest_16x6_210e_coco_raubtierv2/20201230_192052copy.log.json"
 
+#Centripetal RTX 3090
+#configfile="/media/felice/RED/FFHS_BT/_TrainedModels/PC03LIN_3090/centripetalnet_hourglass104_mstest_16x6_210e_coco_raubtierv2/centripetalnet_hourglass104_mstest_16x6_210e_coco_raubtierv2.py"
+#checkpointfile="/media/felice/RED/FFHS_BT/_TrainedModels/PC03LIN_mmdetection/centripetalnet_hourglass104_mstest_16x6_210e_coco_raubtierv2/epoch_11.pth"
+#logfile="/media/felice/RED/FFHS_BT/_TrainedModels/PC03LIN_3090/centripetalnet_hourglass104_mstest_16x6_210e_coco_raubtierv2/20210120_235920.log.json"
+
+
 # DCN
 #configfile="./configs/raubtierv2/cascade_rcnn_r101_fpn_dconv_c3-c5_1x_coco_raubtierv2.py"
 #checkpointfile="/media/felice/RED/FFHS_BT/_TrainedModels/PC03LIN_mmdetection/cascade_rcnn_r101_fpn_dconv_c3-c5_1x_coco_raubtierv2_20200104_30ep/epoch_27.pth"
 #logfile="customEvaluation/cascade_rcnn_r101_fpn_dconv_c3-c5_1x_coco_raubtierv2_20210104_163131.log.json"
 
-# DetectoRS
+# DetectoRS Raubtierv2
 #configfile="./configs/raubtierv2/detectors_cascade_rcnn_r50_1x_coco_raubtierv2.py"
 #checkpointfile="/media/felice/RED/FFHS_BT/_TrainedModels/detectors_cascade_rcnn_r50_esfDSVM01B_20201231/detectors_cascade_rcnn_r50_1x_coco_raubtierv2/epoch_11.pth"
 #logfile="/media/felice/RED/FFHS_BT/_TrainedModels/detectors_cascade_rcnn_r50_esfDSVM01B_20201231/detectors_cascade_rcnn_r50_1x_coco_raubtierv2//20201230_161644.log.json"
 
+#DetectoRS Caltech clean
+#configfile="./configs/raubtierv2/detectors_cascade_rcnn_r50_1x_coco_raubtierv2.py"
+#checkpointfile="/media/felice/RED/FFHS_BT/_TrainedModels/detectors_cascade_rcnn_r50_esfDSVM01B_20201231/detectors_cascade_rcnn_r50_1x_coco_raubtierv2/epoch_11.pth"
+logfile="/home/felice/Work/Git_Ext/esf-bt2020_mmdetection/work_dirs/detectors_cascade_rcnn_r50_1x_coco_caltech_clean/20210131_003235.log.json"
+
+
+
+
 # HRNet
-configfile="./configs/raubtierv2/cascade_rcnn_hrnetv2p_w40_20e_coco_raubtierv2.py"
-checkpointfile="/media/felice/RED/FFHS_BT/_TrainedModels/colabwork-2021-01-0418-09-09_hrnet/content/esf-bt2020_mmdetection/work_dirs/cascade_rcnn_hrnetv2p_w40_20e_coco_raubtierv2/epoch_14.pth"
-logfile="/media/felice/RED/FFHS_BT/_TrainedModels/colabwork-2021-01-0418-09-09_hrnet/content/esf-bt2020_mmdetection/work_dirs/cascade_rcnn_hrnetv2p_w40_20e_coco_raubtierv2/20210104_145037.log.json"
+#configfile="./configs/raubtierv2/cascade_rcnn_hrnetv2p_w40_20e_coco_raubtierv2.py"
+#checkpointfile="/media/felice/RED/FFHS_BT/_TrainedModels/colabwork-2021-01-0418-09-09_hrnet/content/esf-bt2020_mmdetection/work_dirs/cascade_rcnn_hrnetv2p_w40_20e_coco_raubtierv2/epoch_14.pth"
+#logfile="/media/felice/RED/FFHS_BT/_TrainedModels/colabwork-2021-01-0418-09-09_hrnet/content/esf-bt2020_mmdetection/work_dirs/cascade_rcnn_hrnetv2p_w40_20e_coco_raubtierv2/20210104_145037.log.json"
 
 # GA Faster
 #configfile="./configs/raubtierv2/ga_faster_x101_64x4d_fpn_1x_coco_raubtierv2.py"
@@ -46,11 +60,11 @@ cd ..
  
 #Training => bbox_mAP plotten
 #echo python tools/analyze_logs.py plot_curve $logfile --keys bbox_mAP #–-legend bbox_mAP
-python tools/analyze_logs.py plot_curve $logfile --keys bbox_mAP #–-legend bbox_mAP
+#python tools/analyze_logs.py plot_curve $logfile --keys bbox_mAP #–-legend bbox_mAP
  
 
 #Training => loss_cls und loss_bbox plotten
-#python tools/analyze_logs.py plot_curve $logfile --keys loss_cls loss_bbox --legend loss_cls loss_bbox
+python tools/analyze_logs.py plot_curve $logfile --keys loss_cls loss_bbox --legend loss_cls loss_bbox
  
 
 #Testdaten anzeigen
