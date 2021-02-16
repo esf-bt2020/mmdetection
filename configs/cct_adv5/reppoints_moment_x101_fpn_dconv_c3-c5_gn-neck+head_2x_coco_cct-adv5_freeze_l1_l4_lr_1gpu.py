ba0f9e7adc0fe568098d8bf1e5c3af7c74fd4bb3
Felice <felice.estermann@gmx.ch>
2021-02-16 11:08:38 +0100

    add reppoints config for cct-adv5

---
+++ configs/cct_adv5/reppoints_moment_x101_fpn_dconv_c3-c5_gn-neck+head_2x_coco_cct-adv5_freeze_l1_l4_lr_1gpu.py
@@ -0,0 +1,49 @@
+# Basiskonfigurationsfile
+_base_ = '../reppoints/reppoints_moment_x101_fpn_dconv_c3-c5_gn-neck+head_2x_coco.py'
+
+model = dict(
+	backbone=dict(
+       
+        	num_stages=4,
+        	frozen_stages=4
+        ),
+    	bbox_head=dict(
+        	num_classes=10)
+       )
+
+
+dataset_type = 'CocoDataset'
+classes = ('racoon', 'squirrel', 'bobcat', 'dog', 'coyote', 'rabbit', 'bird', 'cat', 'deer', 'fox') #10 classes
+
+data = dict(
+    train=dict(
+    	type='CocoDataset',
+        #img_prefix='customDataCaltech/caltech_halfsize_adv',
+        #img_prefix='/media/Pool/Thesis/Datensets/cct_images',
+ 	img_prefix='/home/felice/Datasets/cct_images',
+        classes=classes,
+        ann_file='customDataCaltech/adv5/adv5_train.json'),
+    val=dict(
+    	type='CocoDataset',
+        #img_prefix='/media/Pool/Thesis/Datensets/cct_images',
+	img_prefix='/home/felice/Datasets/cct_images',
+        classes=classes,
+        ann_file='customDataCaltech/adv5/adv5_known_val.json'),
+    test=dict(
+    	type='CocoDataset',
+	img_prefix='/home/felice/Datasets/cct_images',
+        #img_prefix='/media/Pool/Thesis/Datensets/cct_images',
+        classes=classes,
+        ann_file='customDataCaltech/adv5/adv5_known_test.json'))
+
+#optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001) #original (8x2=16)
+optimizer = dict(type='SGD', lr=0.0025, momentum=0.9, weight_decay=0.0001) #(1x2=2)
+
+total_epochs = 16 #default=12
+
+evaluation = dict(classwise=True, interval=1, metric='bbox')
+
+load_from = 'checkpoints/reppoints_moment_x101_fpn_dconv_c3-c5_gn-neck%2Bhead_2x_coco_20200329-f87da1ea.pth'
+
+#http://download.openmmlab.com/mmdetection/v2.0/reppoints/reppoints_moment_x101_fpn_dconv_c3-c5_gn-neck%2Bhead_2x_coco/reppoints_moment_x101_fpn_dconv_c3-c5_gn-neck%2Bhead_2x_coco_20200329-f87da1ea.pth
+
