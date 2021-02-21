2bdb0447319090283ee93b3e98ce89d0d56f463e
Felice <felice.estermann@gmx.ch>
2021-02-21 14:39:23 +0100

    add detail evaluation script adv5v2

---
+++ customEvaluation/customEval_cctadv5_detail_intern.sh
@@ -0,0 +1,56 @@
+
+
+echo Params: 1=folderImagesPath, 2=fileTransTest, 3=folderModelPath, 4=bestEpochKnown, 5=bestEpochTrans 
+
+
+
+folderImages=$1
+fileAnnotationsTransTest=$2
+folderModel=$3
+
+bestEpochKnown=$4
+bestEpochTrans=$5
+
+modelConfig="${folderModel}/train_config.py"
+
+
+
+outputfolderknown="${folderModel}/test_known"
+echo Creating Folder: $outputfolderknown
+mkdir $outputfolderknown
+fileEpoch="${folderModel}/epoch_${bestEpochKnown}.pth"
+python tools/test.py $modelConfig $fileEpoch --show-dir $outputfolderknown --eval bbox > "${folderModel}/test_known_best_ep${i}.log"
+
+
+
+outputfoldertrans="${folderModel}/test_trans"
+echo Creating Folder: $outputfoldertrans
+mkdir $outputfoldertrans
+fileEpoch="${folderModel}/epoch_${bestEpochTrans}.pth"
+python tools/test.py $modelConfig $fileEpoch --show-dir $outputfoldertrans --cfg-options data.test.ann_file=$fileAnnotationsTransTest --eval bbox > "${folderModel}/test_trans_best_ep${i}.log"
+
+
+#Save Test
+#python tools/test.py $configfile $checkpointfile --out resultfile.pkl
+
+
+#Evaluierung auf Testdatenset
+#python ./tools/test.py $configfile $checkpointfile --eval bbox
+ 
+#Training => bbox_mAP plotten
+#echo python tools/analyze_logs.py plot_curve $logfile --keys bbox_mAP #–-legend bbox_mAP
+#python tools/analyze_logs.py plot_curve $logfile --keys bbox_mAP #–-legend bbox_mAP
+ 
+
+#Training => loss_cls und loss_bbox plotten
+#python tools/analyze_logs.py plot_curve $logfile --keys loss_cls loss_bbox --legend loss_cls loss_bbox
+ 
+
+#Testdaten anzeigen
+
+# python ./tools/test.py \
+# $configfile \
+# $checkpointfile \
+# --out resultfile.pkl \
+# --show
+
---
+++ customEvaluation/customEval_cctadv5_test_detail_pc01.sh
@@ -0,0 +1,14 @@
+
+echo Params: 1=folderModelPath, 2=bestEpochKnown, 3=bestEpochTrans 
+
+
+folderImages="/home/felice/Datasets/cct_images"
+fileAnnotationsTransTest='customDataCaltech/adv5/adv5_trans_test.json'
+folderModel=$1
+
+bestEpochKnown=$2
+bestEpochTrans=$3
+
+#echo customEval_cctadv5_test_detail_intern.sh Params: 1=folderImagesPath, 2=fileTransTest, 3=folderModelPath, 4=bestEpochKnown, 5=bestEpochTrans 
+customEvaluation/customEval_cctadv5_detail_intern.sh $folderImages $fileAnnotationsTransTest $folderModel $bestEpochKnown $bestEpochTrans
+
