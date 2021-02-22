1e84e2541b5b9eb53dd780ba941eab07117adf1d
Felice <felice.estermann@gmx.ch>
2021-02-22 19:18:17 +0100

    add cascade x101 cct_adv5v2 config for azure

---
+++ configs/cct_adv5v2/cascade_rcnn_x101_64x4d_fpn_20e_coco_cct-adv5v2_freeze_l1_l4.py
@@ -0,0 +1,102 @@
+# The new config inherits a base config to highlight the necessary modification
+_base_ = '../cascade_rcnn/cascade_rcnn_x101_64x4d_fpn_20e_coco.py'
+
+dataset_type = 'CocoDataset'
+classes = ('bobcat', 'coyote', 'raccoon', 'bird', 'dog', 'cat', 'squirrel','rabbit', 'deer', 'fox') #10 classes
+
+model = dict(
+
+backbone=dict(
+		frozen_stages=4
+		),
+
+    roi_head=dict(
+        bbox_head=[
+            dict(
+                type='Shared2FCBBoxHead',
+                in_channels=256,
+                fc_out_channels=1024,
+                roi_feat_size=7,
+                num_classes=10,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=[0., 0., 0., 0.],
+                    target_stds=[0.1, 0.1, 0.2, 0.2]),
+                reg_class_agnostic=True,
+                loss_cls=dict(
+                    type='CrossEntropyLoss',
+                    use_sigmoid=False,
+                    loss_weight=1.0),
+                loss_bbox=dict(type='SmoothL1Loss', beta=1.0,
+                               loss_weight=1.0)),
+            dict(
+                type='Shared2FCBBoxHead',
+                in_channels=256,
+                fc_out_channels=1024,
+                roi_feat_size=7,
+                num_classes=10,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=[0., 0., 0., 0.],
+                    target_stds=[0.05, 0.05, 0.1, 0.1]),
+                reg_class_agnostic=True,
+                loss_cls=dict(
+                    type='CrossEntropyLoss',
+                    use_sigmoid=False,
+                    loss_weight=1.0),
+                loss_bbox=dict(type='SmoothL1Loss', beta=1.0,
+                               loss_weight=1.0)),
+            dict(
+                type='Shared2FCBBoxHead',
+                in_channels=256,
+                fc_out_channels=1024,
+                roi_feat_size=7,
+                num_classes=10,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=[0., 0., 0., 0.],
+                    target_stds=[0.033, 0.033, 0.067, 0.067]),
+                reg_class_agnostic=True,
+                loss_cls=dict(
+                    type='CrossEntropyLoss',
+                    use_sigmoid=False,
+                    loss_weight=1.0),
+                loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0))
+        ]
+        
+        
+        ))
+
+
+data = dict(
+    train=dict(
+    	type='CocoDataset',
+        #img_prefix='customDataCaltech/caltech_halfsize_adv',
+        #img_prefix='/media/Pool/Thesis/Datensets/cct_images',
+ 	#img_prefix='/home/felice/Datasets/cct_images',
+ 	img_prefix='/home/azureuser/cct_images_bbox_all',
+        classes=classes,
+        ann_file='customDataCaltech/adv5/adv5_train.json'),
+    val=dict(
+    	type='CocoDataset',
+        #img_prefix='/media/Pool/Thesis/Datensets/cct_images',
+	#img_prefix='/home/felice/Datasets/cct_images',
+	img_prefix='/home/azureuser/cct_images_bbox_all',
+        classes=classes,
+        ann_file='customDataCaltech/adv5/adv5_known_val.json'),
+    test=dict(
+    	type='CocoDataset',
+	#img_prefix='/home/felice/Datasets/cct_images',
+        #img_prefix='/media/Pool/Thesis/Datensets/cct_images',
+        img_prefix='/home/azureuser/cct_images_bbox_all',
+        classes=classes,
+        ann_file='customDataCaltech/adv5/adv5_known_test.json'))
+        
+#optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001) #original (8x2=16)
+optimizer = dict(type='SGD', lr=0.0025, momentum=0.9, weight_decay=0.0001) #(1x2=2)
+
+evaluation = dict(classwise=True, interval=1, metric='bbox')
+
+
+# Use the pre-trained model to obtain higher performance
+load_from = 'checkpoints/cascade_rcnn_x101_64x4d_fpn_20e_coco_20200509_224357-051557b1.pth'
