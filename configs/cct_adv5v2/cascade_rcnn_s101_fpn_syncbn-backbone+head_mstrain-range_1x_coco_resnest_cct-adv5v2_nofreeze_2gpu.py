75b5e2a90e3b922cdf7583a6682bb4b38db9db10
Felice <felice.estermann@gmx.ch>
2021-02-26 11:18:55 +0100

    add resnest cctadv5v2 cheetah 2 gpu no freeze

---
+++ configs/cct_adv5v2/cascade_rcnn_s101_fpn_syncbn-backbone+head_mstrain-range_1x_coco_resnest_cct-adv5v2_nofreeze_2gpu.py
@@ -0,0 +1,125 @@
+# Basiskonfigurationsfile
+_base_ = '../resnest/cascade_rcnn_s101_fpn_syncbn-backbone+head_mstrain-range_1x_coco.py'
+
+dataset_type = 'CocoDataset'
+classes = ('bobcat', 'coyote', 'raccoon', 'bird', 'dog', 'cat', 'squirrel','rabbit', 'deer', 'fox') #10 classes
+
+model = dict(
+
+	backbone=dict(
+		norm_cfg=dict(type='BN', requires_grad=True),
+		frozen_stages=1
+	
+	),
+
+	roi_head=dict(
+	
+		bbox_head=[
+            dict(
+                type='Shared4Conv1FCBBoxHead',
+                in_channels=256,
+                conv_out_channels=256,
+                fc_out_channels=1024,
+                norm_cfg=dict(type='BN', requires_grad=True),
+                roi_feat_size=7,
+                num_classes=10,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=[0.0, 0.0, 0.0, 0.0],
+                    target_stds=[0.1, 0.1, 0.2, 0.2]),
+                reg_class_agnostic=True,
+                loss_cls=dict(
+                    type='CrossEntropyLoss',
+                    use_sigmoid=False,
+                    loss_weight=1.0),
+                loss_bbox=dict(type='SmoothL1Loss', beta=1.0,
+                               loss_weight=1.0)),
+            dict(
+                type='Shared4Conv1FCBBoxHead',
+                in_channels=256,
+                conv_out_channels=256,
+                fc_out_channels=1024,
+                norm_cfg=dict(type='BN', requires_grad=True),
+                roi_feat_size=7,
+                num_classes=10,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=[0.0, 0.0, 0.0, 0.0],
+                    target_stds=[0.05, 0.05, 0.1, 0.1]),
+                reg_class_agnostic=True,
+                loss_cls=dict(
+                    type='CrossEntropyLoss',
+                    use_sigmoid=False,
+                    loss_weight=1.0),
+                loss_bbox=dict(type='SmoothL1Loss', beta=1.0,
+                               loss_weight=1.0)),
+            dict(
+                type='Shared4Conv1FCBBoxHead',
+                in_channels=256,
+                conv_out_channels=256,
+                fc_out_channels=1024,
+                norm_cfg=dict(type='BN', requires_grad=True),
+                roi_feat_size=7,
+                num_classes=10,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=[0.0, 0.0, 0.0, 0.0],
+                    target_stds=[0.033, 0.033, 0.067, 0.067]),
+                reg_class_agnostic=True,
+                loss_cls=dict(
+                    type='CrossEntropyLoss',
+                    use_sigmoid=False,
+                    loss_weight=1.0),
+                loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0))
+        	]
+	
+	
+	)
+
+
+)
+
+norm_cfg = dict(type='BN', requires_grad=True)
+
+data = dict(
+    train=dict(
+    	type='CocoDataset',
+        #img_prefix='customDataCaltech/caltech_halfsize_adv',
+        #img_prefix='/media/Pool/Thesis/Datensets/cct_images',
+ 	#img_prefix='/home/felice/Datasets/cct_images',
+	#img_prefix='/home/azureuser/cct_images_bbox_all',
+ 	img_prefix='/media/SSD2project/WilLiCam/datasets/caltech/cct_images',
+        classes=classes,
+        ann_file='customDataCaltech/adv5/adv5_train.json'),
+    val=dict(
+    	type='CocoDataset',
+        #img_prefix='/media/Pool/Thesis/Datensets/cct_images',
+	#img_prefix='/home/felice/Datasets/cct_images',
+	img_prefix='/media/SSD2project/WilLiCam/datasets/caltech/cct_images',
+	#img_prefix='/home/azureuser/cct_images_bbox_all',
+        classes=classes,
+        ann_file='customDataCaltech/adv5/adv5_known_val.json'),
+    test=dict(
+    	type='CocoDataset',
+	#img_prefix='/home/felice/Datasets/cct_images',
+        #img_prefix='/media/Pool/Thesis/Datensets/cct_images',
+        img_prefix='/media/SSD2project/WilLiCam/datasets/caltech/cct_images',
+	#img_prefix='/home/azureuser/cct_images_bbox_all',
+        classes=classes,
+        ann_file='customDataCaltech/adv5/adv5_known_test.json'))
+    
+#total_epochs = 30
+
+#optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001) #original (8x2=16)
+#optimizer = dict(type='SGD', lr=0.0025, momentum=0.9, weight_decay=0.0001) #(1x2=2)
+optimizer = dict(type='SGD', lr=0.005, momentum=0.9, weight_decay=0.0001) #cheetah (2x2=4)
+
+evaluation = dict(classwise=True, interval=1, metric='bbox')
+
+# Pretrained model laden
+
+load_from = 'checkpoints/cascade_rcnn_s101_fpn_syncbn-backbone%2Bhead_mstrain-range_1x_coco_20201005_113242-b9459f8f.pth'
+
+#http://download.openmmlab.com/mmdetection/v2.0/resnest/cascade_rcnn_s101_fpn_syncbn-backbone%2Bhead_mstrain-range_1x_coco/cascade_rcnn_s101_fpn_syncbn-backbone%2Bhead_mstrain-range_1x_coco_20201005_113242-b9459f8f.pth
+
+work_dir = '/media/storage1/projects/WilLiCam/checkpoint_workdir/cascade_rcnn_s101_fpn_syncbn-backbone+head_mstrain-range_1x_coco_resnest_cct-adv5v2_nofreeze_2gpu'
