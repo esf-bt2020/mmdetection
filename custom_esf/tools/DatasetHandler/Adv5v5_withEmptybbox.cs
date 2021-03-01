9d7f2d5fbe93609952c9b99fabb82d489dee5863
Felice <felice.estermann@gmx.ch>
2021-03-01 22:25:10 +0100

    updated datasethandler adv5v5

--- custom_esf/tools/DatasetHandler/Adv5v5_withEmptybbox.cs
+++ custom_esf/tools/DatasetHandler/Adv5v5_withEmptybbox.cs
@@ -7,11 +7,11 @@ using System.Text.Json;
 
 namespace DatasetHandler
 {
-    public class Adv5v3_withEmpty
+    public class Adv5v5_withEmptybbox
     {
         private Coco _full;
 
-        public Adv5v3_withEmpty(string full)
+        public Adv5v5_withEmptybbox(string full)
         {
             string jsonString = File.ReadAllText(full);
             _full = JsonSerializer.Deserialize<Coco>(jsonString);
@@ -61,16 +61,16 @@ namespace DatasetHandler
             }
 
             string sourcePath = @"C:\Work\Github\esf-bt2020_mmdetection\customDataCaltech\adv5";
-            string targetPath = @"C:\Work\Github\esf-bt2020_mmdetection\customDataCaltech\adv5v3";
+            string targetPath = @"C:\Work\Github\esf-bt2020_mmdetection\customDataCaltech\adv5v5";
 
 
 
-            AppendImages(sourcePath, targetPath, "adv5_train.json", "adv5v3_train.json", knowntrainImages);
-            AppendImages(sourcePath, targetPath, "adv5_known_val.json", "adv5v3_known_val.json", knownvalImages);
-            AppendImages(sourcePath, targetPath, "adv5_known_test.json", "adv5v3_known_test.json", knowntestImages);
+            AppendImages(sourcePath, targetPath, "adv5_train.json", "adv5v5_train.json", knowntrainImages);
+            AppendImages(sourcePath, targetPath, "adv5_known_val.json", "adv5v5_known_val.json", knownvalImages);
+            AppendImages(sourcePath, targetPath, "adv5_known_test.json", "adv5v5_known_test.json", knowntestImages);
             
-            AppendImages(sourcePath, targetPath, "adv5_trans_val.json", "adv5v3_trans_val.json", transValImages);
-            AppendImages(sourcePath, targetPath, "adv5_trans_test.json", "adv5v3_trans_test.json", transTestImages);
+            AppendImages(sourcePath, targetPath, "adv5_trans_val.json", "adv5v5_trans_val.json", transValImages);
+            AppendImages(sourcePath, targetPath, "adv5_trans_test.json", "adv5v5_trans_test.json", transTestImages);
 
 
 
@@ -81,14 +81,33 @@ namespace DatasetHandler
             string jsonString = File.ReadAllText(Path.Combine(sourceFolder, sourceFilename));
             var deserialized = JsonSerializer.Deserialize<Coco>(jsonString);
 
+            var allannotations = deserialized.annotations.ToList();
+
             var allimages = deserialized.images.ToList();
 
+            foreach(var newimage in imagesToAdd )
+            {
+                allimages.Add(newimage);
+
+                Annotation newanno = new Annotation();
+                newanno.image_id = newimage.id;
+                newanno.area = 0;
+                newanno.bbox = new double[4] { 0, 0, 0, 0 };
+                newanno.iscrowd = 0;
+                newanno.id = newanno.image_id + "empty";
+                newanno.category_id = 51; //set empty bbox to 51=>fox
 
 
-            allimages.AddRange(imagesToAdd.ToArray());
+                allannotations.Add(newanno);
+            }
 
+            var categories = deserialized.categories.ToList();
+            //categories.Add(new Category() { id = 0, name = "customEmpty", supercategory = null });
+            deserialized.categories = categories.ToArray();
 
             deserialized.images = allimages.ToArray();
+            deserialized.annotations = allannotations.ToArray();
+
             var serialized = JsonSerializer.Serialize(deserialized, typeof(Coco));
             System.IO.File.WriteAllText(Path.Combine(targetFolder, targetFilename), serialized);
         }
