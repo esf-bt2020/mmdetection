642f50e9af7587d6cb5329a6502620889bcaa270
Felice <felice.estermann@gmx.ch>
2021-03-02 14:20:41 +0100

    optimize evalution script best epochs

--- customEvaluation/customEval_cctadv5_detail_intern.sh
+++ customEvaluation/customEval_cctadv5_detail_intern.sh
@@ -19,7 +19,7 @@ outputfolderknown="${folderModel}/test_known"
 echo Creating Folder: $outputfolderknown
 mkdir $outputfolderknown
 fileEpoch="${folderModel}/epoch_${bestEpochKnown}.pth"
-python tools/test.py $modelConfig $fileEpoch --show-dir $outputfolderknown --eval bbox > "${folderModel}/test_known_best_ep${i}.log"
+python tools/test.py $modelConfig $fileEpoch --show-dir $outputfolderknown --eval bbox > "${folderModel}/test_known_best_ep${bestEpochKnown}.log"
 
 
 
@@ -27,7 +27,7 @@ outputfoldertrans="${folderModel}/test_trans"
 echo Creating Folder: $outputfoldertrans
 mkdir $outputfoldertrans
 fileEpoch="${folderModel}/epoch_${bestEpochTrans}.pth"
-python tools/test.py $modelConfig $fileEpoch --show-dir $outputfoldertrans --cfg-options data.test.ann_file=$fileAnnotationsTransTest --eval bbox > "${folderModel}/test_trans_best_ep${i}.log"
+python tools/test.py $modelConfig $fileEpoch --show-dir $outputfoldertrans --cfg-options data.test.ann_file=$fileAnnotationsTransTest --eval bbox > "${folderModel}/test_trans_best_ep${bestEpochTrans}.log"
 
 
 #Save Test
