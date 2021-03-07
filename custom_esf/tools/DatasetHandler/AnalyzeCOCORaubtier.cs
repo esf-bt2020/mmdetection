0abef12bcbc72c372987d07ebf2c633912d7abb5
Felice <felice.estermann@gmx.ch>
2021-03-07 21:31:34 +0100

    add detectoRS 4gpu nofreeze cctadv5v2

---
+++ custom_esf/tools/DatasetHandler/AnalyzeCOCORaubtier.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace DatasetHandler
+{
+    public class AnalyzeCOCORaubtier
+    {
+        public static void AnalyzeDataset()
+        {
+            string train = @"C:\Data\FFHS\BT\raubtierv2_b\train\_annotations.coco.json";
+            string valid = @"C:\Data\FFHS\BT\raubtierv2_b\valid\_annotations.coco.json";
+            string test = @"C:\Data\FFHS\BT\raubtierv2_b\test\_annotations.coco.json";
+            CreateFullStatistic(train);
+            CreateFullStatistic(valid);
+            CreateFullStatistic(test);
+
+        }
+        public static void CreateFullStatistic(string file)
+        {
+            string jsonString = File.ReadAllText(file);
+            var deserialized = JsonSerializer.Deserialize<COCORaubtierV2.COCORaubtierV2>(jsonString);
+
+
+            StringBuilder stringBuilder = new StringBuilder();
+            int imgwithbbox = 0;
+
+            int cat1annocounter = 0;
+            int cat2annocounter = 0;
+            int cat3annocounter =0;
+
+            int cat1imgcounter = 0;
+            int cat2imgcounter = 0;
+            int cat3imgcounter = 0;
+
+            foreach (var image in deserialized.images )
+            {
+                int annoCounter = 0;
+                long? category = null;
+                foreach(var anno in deserialized.annotations )
+                {
+                    if ( anno.image_id == image.id )
+                    {
+                        annoCounter++;
+                        switch(anno.category_id )
+                        {
+                            case 1:
+                                cat1annocounter++;
+                                break;
+                            case 2:
+                                cat2annocounter++;
+                                break;
+                            case 3:
+                                cat3annocounter++;
+                                break;
+                        }
+                        if ( category == null )
+                        {
+                            category = anno.category_id;
+                            switch (category)
+                            {
+                                case 1:
+                                    cat1imgcounter++;
+                                    break;
+                                case 2:
+                                    cat2imgcounter++;
+                                    break;
+                                case 3:
+                                    cat3imgcounter++;
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            if (category != anno.category_id)
+                            {
+                                Console.WriteLine("Fehler!");
+                            }
+                        }
+                    }
+                }
+                if (annoCounter > 0 )
+                {
+                    imgwithbbox++;
+                }
+            }
+
+            string output = string.Empty;
+
+            stringBuilder.Append("CategoryId");
+
+
+            string result = stringBuilder.ToString();
+        }
+    }
+
+}
---
+++ custom_esf/tools/DatasetHandler/COCOForRaubtierV2.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace DatasetHandler.COCORaubtierV2
+    {
+
+
+        public partial class COCORaubtierV2
+        {
+            public Info info { get; set; }
+
+
+            public License[] licenses { get; set; }
+
+
+            public Category[] categories { get; set; }
+
+
+            public Image[] images { get; set; }
+
+
+            public Annotation[] annotations { get; set; }
+        }
+
+        public partial class Annotation
+        {
+
+            public long id { get; set; }
+
+            public string id_caltech { get; set; }
+
+
+            public long image_id { get; set; }
+
+            public string image_id_caltech { get; set; }
+
+
+            public long category_id { get; set; }
+
+
+            public double[] bbox { get; set; }
+
+
+            public double area { get; set; }
+
+
+            public object[] segmentation { get; set; }
+
+
+            public long iscrowd { get; set; }
+        }
+
+        public partial class Category
+        {
+
+            public long id { get; set; }
+
+            public string name { get; set; }
+
+            public string supercategory { get; set; }
+        }
+
+        public partial class Image
+        {
+            public long id { get; set; }
+
+            public string id_caltech { get; set; }
+
+            public long license { get; set; }
+
+            public string file_name { get; set; }
+
+
+            public long height { get; set; }
+
+            public long width { get; set; }
+
+            public string date_captured { get; set; }
+
+            //Added Properties
+            public string location { get; set; } //je nach File  string oder long
+            public long seq_num_frames { get; set; }
+            public long frame_num { get; set; }
+
+            public string seq_id { get; set; }
+        }
+
+        public partial class Info
+        {
+
+            public string year { get; set; }
+
+
+            public string version { get; set; }
+
+            public string description { get; set; }
+
+            public string contributor { get; set; }
+
+            public Uri url { get; set; }
+
+            public string date_created { get; set; }
+        }
+
+        public partial class License
+        {
+            public long id { get; set; }
+
+            public string url { get; set; }
+
+            public string name { get; set; }
+        }
+    }
+
+
+
