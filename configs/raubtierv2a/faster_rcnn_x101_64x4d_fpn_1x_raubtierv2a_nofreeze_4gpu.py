25c390b673adc7102c7011774f96a5ced3b697c5
Felice <felice.estermann@gmx.ch>
2021-03-14 02:02:40 +0100

    add nofreeze configs

---
+++ configs/raubtierv2a/faster_rcnn_x101_64x4d_fpn_1x_raubtierv2a_nofreeze_4gpu.py
@@ -0,0 +1,55 @@
+_base_ = '../faster_rcnn/faster_rcnn_x101_64x4d_fpn_1x_coco.py'
+
+
+model = dict(
+
+backbone=dict(
+       
+        	num_stages=4,
+        	#frozen_stages=4
+        	),
+
+	roi_head=dict(
+		
+		bbox_head=dict(
+
+			num_classes=3
+				
+		)
+
+
+
+	)
+)
+
+
+dataset_type = 'COCODataset'
+classes = ('luchs', 'rotfuchs', 'wolf')
+data = dict(
+    train=dict(
+        img_prefix='raubtierv2a/train/',
+        classes=classes,
+        ann_file='raubtierv2a/train/_annotations.coco.json'),
+    val=dict(
+        img_prefix='raubtierv2a/valid/',
+        classes=classes,
+        ann_file='raubtierv2a/valid/_annotations.coco.json'),
+    test=dict(
+        img_prefix='raubtierv2a/test/',
+        classes=classes,
+        ann_file='raubtierv2a/test/_annotations.coco.json'))
+        
+#optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001) #original (8x2=16)
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001) #(4x2=8) 4 GPUs
+#optimizer = dict(type='SGD', lr=0.0025, momentum=0.9, weight_decay=0.0001) #(1x2=2)
+
+total_epochs=24
+
+evaluation = dict(classwise=True, interval=1, metric='bbox')
+
+work_dir = '/media/storage1/projects/WilLiCam/checkpoint_workdir/raubtierv2a/faster_rcnn_x101_64x4d_fpn_1x_raubtierv2a_nofreeze_4gpu'
+
+#http://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_x101_64x4d_fpn_1x_coco/faster_rcnn_x101_64x4d_fpn_1x_coco_20200204-833ee192.pth
+
+
+load_from = 'checkpoints/faster_rcnn_x101_64x4d_fpn_1x_coco_20200204-833ee192.pth'
---
+++ configs/raubtierv2a/reppoints_moment_x101_fpn_dconv_c3-c5_gn-neck+head_2x_coco_raubtierv2a_nofreeze_lr_4gpu.py
@@ -0,0 +1,45 @@
+# Basiskonfigurationsfile
+_base_ = '../reppoints/reppoints_moment_x101_fpn_dconv_c3-c5_gn-neck+head_2x_coco.py'
+
+model = dict(
+	backbone=dict(
+       
+        	num_stages=4,
+        	#frozen_stages=4
+        ),
+    	bbox_head=dict(
+        	num_classes=3)
+       )
+
+
+
+dataset_type = 'COCODataset'
+classes = ('luchs', 'rotfuchs', 'wolf')
+data = dict(
+    train=dict(
+        img_prefix='raubtierv2a/train/',
+        classes=classes,
+        ann_file='raubtierv2a/train/_annotations.coco.json'),
+    val=dict(
+        img_prefix='raubtierv2a/valid/',
+        classes=classes,
+        ann_file='raubtierv2a/valid/_annotations.coco.json'),
+    test=dict(
+        img_prefix='raubtierv2a/test/',
+        classes=classes,
+        ann_file='raubtierv2a/test/_annotations.coco.json'))
+
+#optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001) #original (8x2=16)
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001) #(4x2=8) 4 GPUs
+#optimizer = dict(type='SGD', lr=0.0025, momentum=0.9, weight_decay=0.0001) #(1x2=2)
+
+total_epochs = 24 #default=12
+
+evaluation = dict(classwise=True, interval=1, metric='bbox')
+
+load_from = 'checkpoints/reppoints_moment_x101_fpn_dconv_c3-c5_gn-neck%2Bhead_2x_coco_20200329-f87da1ea.pth'
+
+work_dir = '/media/storage1/projects/WilLiCam/checkpoint_workdir/raubtierv2a/reppoints_moment_x101_fpn_dconv_c3-c5_gn-neck+head_2x_coco_raubtierv2a_nofreeze_lr_4gpu'
+
+#http://download.openmmlab.com/mmdetection/v2.0/reppoints/reppoints_moment_x101_fpn_dconv_c3-c5_gn-neck%2Bhead_2x_coco/reppoints_moment_x101_fpn_dconv_c3-c5_gn-neck%2Bhead_2x_coco_20200329-f87da1ea.pth
+
